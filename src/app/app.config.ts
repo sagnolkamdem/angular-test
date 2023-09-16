830c7a2e54bb257300e5b1e5306bcbd2beb6fe41
sagnolkamdem <sagnolkamdem721@gmail.com>
2023-09-16 08:21:27 +0100

    :tada: Start app
    start app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
+import { routes } from './core/routes/app.routes';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { environment } from 'environment/environment.prod';
+import { StoreModule } from '@ngrx/store';
+import { ROOT_REDUCERS } from './core/store/app.store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      BrowserModule,
+      BrowserAnimationsModule,
+      EffectsModule.forRoot(),
+      StoreModule.forRoot(ROOT_REDUCERS),
+      StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        logOnly: environment.production,
+      }),
+      StoreRouterConnectingModule.forRoot(),
+    ]),
+  ],
 };
---
+++ src/app/core/components/app/app.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+
+@Component({
+  selector: 'afya-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ButtonModule,
+    CarouselModule,
+    TagModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'afya';
+
+  responsiveOptions: any[] | undefined;
+
+  products = [
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1001',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch 1',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+    {
+      id: '1002',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch 2',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5,
+    },
+  ];
+
+  ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
+  getSeverity(status: string): string {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'warning';
+      case 'OUTOFSTOCK':
+        return 'danger';
+      default:
+        return 'info';
+    }
+  }
+}
